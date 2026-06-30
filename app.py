74805ac8acbc3961e1ccb4f9e5a6ee8177d45441
Dasha <dasha36980@gmail.com>
2026-07-01 02:57:53 +0300

    fix: correct __name__ check in app.py

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 init_db(app)
 app.register_blueprint(main)
 
-if name == '__main__':
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=False, host='0.0.0.0', port=port)
